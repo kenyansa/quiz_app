8c81b9c9abd91851d61e1aefacde445e33610072
Kenyansa <kenyansajustine@gmail.com>
2023-07-24 00:11:02 +0300

    creating variables to store index and score

--- script.js
+++ script.js
@@ -48,3 +48,6 @@ const questions = [
 const questionElement = document.getElementById("question");
 const answerButton = document.getElementById("answer-buttons");
 const nextButton = document.getElementById("next-btn");
+
+let questionIndex =  0;
+let score = 0;
