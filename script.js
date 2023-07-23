c845040115fab4cbaa33bd449bcbca6ccd4f5194
Kenyansa <kenyansajustine@gmail.com>
2023-07-23 23:59:02 +0300

    add variables of questions an answers

---
+++ script.js
@@ -0,0 +1,11 @@
+const questions = [
+    {
+        question: "Qeustion one",
+        answers: [
+            {text: "answer 1", correct: false},
+            {text: "answer 2", correct: true},
+            {text: "answer 3", correct: false},
+            {text: "answer 4", correct: false}
+        ]
+    }
+]
