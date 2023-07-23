83d81246e1304f0ec674771bdec74bc4adee5183
Kenyansa <kenyansajustine@gmail.com>
2023-07-24 00:06:00 +0300

    add more variables for buttons

--- script.js
+++ script.js
@@ -7,5 +7,44 @@ const questions = [
             {text: "answer 3", correct: false},
             {text: "answer 4", correct: false}
         ]
+    },
+    {
+        question: "Qeustion Two",
+        answers: [
+            {text: "answer 1", correct: false},
+            {text: "answer 2", correct: true},
+            {text: "answer 3", correct: false},
+            {text: "answer 4", correct: false}
+        ]
+    },
+    {
+        question: "Qeustion Three",
+        answers: [
+            {text: "answer 1", correct: false},
+            {text: "answer 2", correct: true},
+            {text: "answer 3", correct: false},
+            {text: "answer 4", correct: false}
+        ]
+    },
+    {
+        question: "Qeustion Four",
+        answers: [
+            {text: "answer 1", correct: false},
+            {text: "answer 2", correct: true},
+            {text: "answer 3", correct: false},
+            {text: "answer 4", correct: false}
+        ]
+    },
+    {
+        question: "Qeustion Five",
+        answers: [
+            {text: "answer 1", correct: false},
+            {text: "answer 2", correct: true},
+            {text: "answer 3", correct: false},
+            {text: "answer 4", correct: false}
+        ]
     }
-]
+];
+const questionElement = document.getElementById("question");
+const answerButton = document.getElementById("answer-buttons");
+const nextButton = document.getElementById("next-btn");
