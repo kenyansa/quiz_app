fdce6971b49f837d1de2069d4389b28b1fc0d18f
Kenyansa <kenyansajustine@gmail.com>
2023-07-25 10:57:48 +0300

    allow user to play again without refreshing the page

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+//define array of questions
 const questions = [
     {
         question: "What is Kenya best known for?",
@@ -9,7 +10,7 @@ const questions = [
         ]
     },
     {
-        question: "Which is the deepst fresh water lake in the world?",
+        question: "Which is the deepest fresh water lake in the world?",
         answers: [
             {text: "Lake Baikal", correct: true},
             {text: "Lake Vostok", correct: false},
@@ -30,7 +31,7 @@ const questions = [
         question: "Which of the following never served as a vice or deputy president in Kenya?",
         answers: [
             {text: "Mwai Kibaki", correct: false},
-            {text: "Joseph Murumbi", correct: true},
+            {text: "Joseph Murumbi", correct: false},
             {text: "Josephat Karanja", correct: false},
             {text: "Charles Njonjo", correct: true}
         ]
@@ -45,81 +46,90 @@ const questions = [
         ]
     }
 ];
-const questionElement = document.getElementById("question");
-const answerButtons = document.querySelector(".answer-buttons");
-const nextButton = document.getElementById("next-btn");
+const questionElement = document.getElementById("question"); //holds ref to html element h1 with id 'question' where the current question will be displayed
+const answerButtons = document.querySelector(".answer-buttons"); //holds ref where answer buttons will be displayed
+const nextButton = document.getElementById("next-btn"); //holds ref for next button for moving to the next question
 
+//initializing q-index and score for tracking
 let questionIndex =  0;
 let score = 0;
 
+//function for starting the quiz
 function startQuiz(){
-    questionIndex = 0;
+    questionIndex = 0; //initialize q-index and score to 0
     score = 0;
-    nextButton.innerHTML = "Next";
+    nextButton.innerHTML = "Next"; //set inner HTMLM for nextButton to 'next then display first question
+    nextButton.style.display = "none"; //hide the button initially
     showQuestion();
+    nextButton.addEventListener('click', handleNextButton); // add event listener to the next button.
 }
 
+//function for displaying current question
 function showQuestion(){
-    resetState();
-    let currentQuestion = questions[questionIndex];
+    resetState(); //clears any previous question and answer buttons
+    let currentQuestion = questions[questionIndex]; //retrieve current question based on q-index
     let questionNo = questionIndex + 1;
-    questionElement.innerHTML = questionNo + "." + currentQuestion.question;
+    questionElement.innerHTML = questionNo + "." + currentQuestion.question; //display iterated question
 
     currentQuestion.answers.forEach(answer =>{
         const button = document.createElement("button");
         button.innerHTML = answer.text;
         button.classList.add("btn");
-        answerButtons.appendChild(button);
+        answerButtons.appendChild(button); //create a new button element for each answer and append it to answerButtons
         
         if(answer.correct){
-            button.dataset.correct = answer.correct;
+            button.dataset.correct = answer.correct; //If the answer is correct, it also sets a custom attribute data-correct on the button and sets its value to true.
         }
         button.addEventListener("click", selectAnswer);
     });
 }
 
 function resetState(){
-    nextButton.style.display = "none";
+    nextButton.style.display = "none"; //this function ides nextButton and removes any child elements from answerButtons using while loop
     while(answerButtons.firstChild){
         answerButtons.removeChild(answerButtons.firstChild)
     }
 }
 
-function selectAnswer(e){
+function selectAnswer(e){ //function called upon clicking on answer button and checks if the button has 'true' data-correct
     const selectedBtn = e.target;
     const isCorrect = selectedBtn.dataset.correct == "true";
-    if(isCorrect){
+    if(isCorrect){ //if answerbe correct, add correct class to the button and increment the score
         selectedBtn.classList.add("correct");
         score++;
     }else{
-        selectedBtn.classList.add("incorrect");
+        selectedBtn.classList.add("incorrect"); //if answer incorrect, add incorrect answer to the button
     }
-    Array.from(answerButtons.children).forEach(button =>{
+    Array.from(answerButtons.children).forEach(button =>{ //disable all answer buttons after the selection
         if(button.dataset.correct === "true"){
             button.classList.add("correct");
         }
         button.disabled = true;
     });
-    nextButton.style.display = "block";
+    nextButton.style.display = "block"; //display nextButton
 }
 
-function showScore(){
+function showScore(){ //show final score after answering all questions, resetting all questions and answers
     resetState();
-    questionElement.innerHTML = `You scored ${score} out of ${questions.length}!`;
-    nextButton.innerHTML = `Play Again`;
+    questionElement.innerHTML = `You scored ${score} out of ${questions.length}!`; //in he place of question element/current question, display the innerHTML
     nextButton.style.display = "block";
+    nextButton.removeEventListener('click', handleNextButton);
+    nextButton.addEventListener('click', startQuiz);
 }
-function handleNextButton(){
+function handleNextButton(){ //increment question index and check if there are more quiz to be answered.
     questionIndex++;
-    if(currentQuestion<questions.length){
+    if(questionIndex<questions.length){
         showQuestion();
     }else{
-        showScore();
+        showScore(); //show score if all quiz are answered
+        nextButton.innerHTML = "Play Again!" // Change the text of the button
+        nextButton.removeEventListener('click', handleNextButton); // remove the repvious event listener
+        nextButton.addEventListener("click", startQuiz) // Add a new event listener to start the quiz again
     }
 }
-nextButton.addEventListener('click', ()=>{
-    if(questionIndex < questions.length){
-        handleNextButton();
-    }
-})
-startQuiz();
+// nextButton.addEventListener('click', ()=>{
+//     if(questionIndex < questions.length){
+//         handleNextButton(); //check if there are quiz to be answered upon clicking the button
+//     }
+// })
+startQuiz(); 
